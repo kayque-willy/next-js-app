5d34add5129d4c2a6420a0ea327681191d08e1f3
kayque oliveira <kayque-willy@hotmail.com>
2023-04-17 17:07:07 -0300

    Not Found Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+    return (
+        <>
+            <div>
+                <h1>404 - Não encontrado</h1>
+                <p>Desculpe, não há nada aqui</p>
+            </div>
+        </>
+    );
+};
+
+export default NotFoundPage;
