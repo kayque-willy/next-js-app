105beff186c917f573f904e057eb81c6cb5a73f6
kayque oliveira <kayque-willy@hotmail.com>
2023-04-17 23:54:56 -0300

    Type bug correction

--- src/app/type/page.tsx
+++ src/app/type/page.tsx
@@ -3,12 +3,13 @@
 import { useRouter } from "next/navigation";
 
 async function Type() {
-    // Requisição à API
-    // const response = await fetch('https://www.digi-api.com/api/v1/type');
-    // const types = await response.json();
 
     // Roteamento
     const router = useRouter();
+
+    // Requisição à API
+    const response = await fetch('https://www.digi-api.com/api/v1/type');
+    const types = await response.json();
 
     //Função de retorno
     function backHome() {
@@ -19,7 +20,7 @@ async function Type() {
         <div>
             <button onClick={backHome}>Voltar</button>
             <pre>
-                {/* {JSON.stringify(types, null, 2)} */}
+                {JSON.stringify(types, null, 2)}
             </pre>
         </div>
     )
