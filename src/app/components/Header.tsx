8c01fee609d7d2fe0c4e04b13ea8b9a739e5410b
kayque oliveira <kayque-willy@hotmail.com>
2023-04-21 12:55:08 -0300

    Loading in search

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import { Spinner } from "./Spinner";
+
 // [λ  (Server)  server] - side renders at runtime uses [getInitialProps] or [getServerSideProps]
 export function Header() {
 
+    const [isLoading, setIsLoading] = useState(false);
+
     return (
         <section className="header-search">
-            <form action="/digimon">
+            <form onSubmit={() => { setIsLoading(true) }} action="/digimon">
                 <fieldset>
                     <input className="input-search" type="search" name="search" placeholder="Procure o digimon pelo nome..."
                     />
                 </fieldset>
                 <button type="submit" value="Submit">Buscar</button>
+                {isLoading && <Spinner />}
             </form>
         </section>
     )
